943f9d7e7cbb06b36477c590b17ad8f5c7f219d2
elsa.m <manangelsa07@gmail.com>
2023-10-19 09:44:50 +0200

    define Auteur Interface Service

---
+++ TestAspApi/Services/Interface/IAuteursService.cs
@@ -0,0 +1,20 @@
+﻿using TestAspApi.Commons;
+using TestAspApi.DTOs;
+using TestAspApi.Models;
+
+namespace TestAspApi.Services.Interface
+{
+    public interface IAuteursService
+    {
+        /// <summary>
+        /// Retourne la list de tous les auteurs disponible
+        /// </summary>
+        /// <returns><see cref="IReadOnlyCollection{T}"/> where <seealso cref="T"/> is <see cref="AuteurDTO"/></returns>
+        Task<Reponse<IReadOnlyCollection<AuteurDTO>>> GetAllAsync();
+        Task<Reponse<AuteurDTO>> GetByIdAsync(int id);
+        Task<Reponse<Auteur>> GetByIdWithBooksAsync(int id);
+        Task<Reponse<AuteurDTO>> CreerNouvelAuteurAsync(CreerAuteurDTO creerAuteur);
+        Task<Reponse<AuteurDTO>> ModifierAuteurAsync(int id, EditerAuteurDTO editerAuteur);
+        Task<Reponse<AuteurDTO>> SupprimerAuteurAsync(int id);
+    }
+}
