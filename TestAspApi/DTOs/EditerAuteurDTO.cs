837753870c2a9fe0f622701ff295889511efb788
elsa.m <manangelsa07@gmail.com>
2023-10-19 09:43:28 +0200

    add Edit Auteur DTO

---
+++ TestAspApi/DTOs/EditerAuteurDTO.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TestAspApi.DTOs
+{
+#nullable disable
+    public class EditerAuteurDTO
+    {
+        [Required(ErrorMessage = "Le nom de l'auteur doit toujours avoir une valeur")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Le mail de l'auteur doit toujours avoir une valeur")]
+        public string Email { get; set; }
+    }
+}
