9f3607db764e5d3b5ef51b1ee574039a7577cfae
elsa.m <manangelsa07@gmail.com>
2023-10-19 09:45:26 +0200

    define Livre Interface Service

---
+++ TestAspApi/Services/Interface/ILivresService.cs
@@ -0,0 +1,15 @@
+﻿using TestAspApi.Commons;
+using TestAspApi.DTOs;
+
+namespace TestAspApi.Services.Interface
+{
+    public interface ILivresService
+    {
+        Task<Reponse<IReadOnlyCollection<LivreDTO>>> GetAllAsync();
+        Task<Reponse<LivreDTO>> GetOneAsync(int id);
+        Task<Reponse<LivreDTO>> CreerNouveauLivreAsync(CreateLivreDTO createLivre);
+        Task<Reponse<LivreDTO>> ModifierLivreAsync(int id, CreateLivreDTO modifierLivre);
+        Task<Reponse<LivreDTO>> SupprimerLivreAsync(int id);
+
+    }
+}
