d568137249ee968b206f4d02fbc651f5a12784b8
elsa.m <manangelsa07@gmail.com>
2023-10-19 09:42:23 +0200

    create the model of api's response

---
+++ TestAspApi/Commons/Reponse.cs
@@ -0,0 +1,31 @@
+﻿namespace TestAspApi.Commons
+{
+    public class Reponse<TData> where TData : class
+    {
+        public Reponse(bool isSucceed, string? message)
+        {
+            IsSucceed = isSucceed;
+            Message = message;
+        }
+
+        public Reponse(bool isSucceed, string? message, TData? data)
+        {
+            IsSucceed = isSucceed;
+            Message = message;
+            Data = data;
+        }
+
+        public Reponse(bool isSucceed, string? message, int status, TData? data)
+        {
+            IsSucceed = isSucceed;
+            Message = message;
+            Status = status;
+            Data = data;
+        }
+
+        public bool IsSucceed { get; set; }
+        public string? Message { get; set; }
+        public int Status { get; set; }
+        public TData? Data { get; set; }
+    }
+}
