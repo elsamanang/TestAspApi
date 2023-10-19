0b1bf82ffe36454a771558b2584ec5d820f2e46a
elsa.m <manangelsa07@gmail.com>
2023-10-19 09:47:33 +0200

    add Services and DbContext

---
+++ TestAspApi/Program.cs
@@ -0,0 +1,38 @@
+using Microsoft.EntityFrameworkCore;
+using TestAspApi.Contexts;
+using TestAspApi.Services;
+using TestAspApi.Services.Interface;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("default"));
+});
+
+builder.Services.AddTransient<IAuteursService, AuteursService>();
+builder.Services.AddTransient<ILivresService, LivresService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
