983a6163f0efe8728a958515e4fd34b67a72996c
elsa.m <manangelsa07@gmail.com>
2023-10-19 09:43:00 +0200

    add create livre DTO

---
+++ TestAspApi/DTOs/CreateLivreDTO.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TestAspApi.DTOs
+{
+#nullable disable
+    public class CreateLivreDTO
+    {
+        [Required(AllowEmptyStrings =false, ErrorMessage ="Le livre doit avoir un titre")]
+        [MinLength(length: 3, ErrorMessage ="Le titre doit avoir au moins 3 caracteres")]
+        public string Title { get; set; }
+        public string Description { get; set; }
+        [Required]
+        public int Pages { get; set; }
+        [Required]
+        public int AuteurId { get; set; }
+    }
+}
