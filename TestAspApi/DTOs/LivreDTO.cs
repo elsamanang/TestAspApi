eb75afa6881bbe1bd0c5c271dbbdb88b95fa3272
elsa.m <manangelsa07@gmail.com>
2023-10-19 09:43:40 +0200

    add Livre DTO

---
+++ TestAspApi/DTOs/LivreDTO.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TestAspApi.DTOs
+{
+#nullable disable
+    public class LivreDTO
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        public string Description { get; set; }
+        [Required]
+        public int Pages { get; set; }
+        [Required]
+        public int AuteurID { get; set; }
+        public string AuteurName { get; set; }
+        
+        public string AuteurEmail { get; set; }
+    }
+}
