f8d9746742eca84e5de05c02f86a928686096c22
elsa.m <manangelsa07@gmail.com>
2023-10-19 09:44:05 +0200

    add Auteur model

---
+++ TestAspApi/Models/Auteur.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TestAspApi.Models
+{
+    public class Auteur
+    {
+        public Auteur()
+        {
+            Livres = new HashSet<Livre>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public required string Name { get; set; }
+        public string? Email { get; set; }
+
+        //prop de navigation
+        public virtual ICollection<Livre> Livres { get; set; }
+    }
+}
