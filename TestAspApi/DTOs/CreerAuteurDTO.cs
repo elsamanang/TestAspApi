3e06e56fe6318e025955c0c9e4b87cbb5e2306fa
elsa.m <manangelsa07@gmail.com>
2023-10-19 09:43:12 +0200

    add Create Auteur DTO

---
+++ TestAspApi/DTOs/CreerAuteurDTO.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TestAspApi.DTOs
+{
+#nullable disable
+    public class CreerAuteurDTO
+    {
+        [MinLength(length: 3, ErrorMessage = "Le nom doit avoir 3 charateres ou plus")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Veuillez saisir le nom de l'auteur")]
+        public string Name { get; set; }
+
+        [EmailAddress(ErrorMessage ="Saisissez une addresse mail valide")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "L'email est obligatoir. car nous devons etre en mesure de vous contacter")]
+        public string Email { get; set; }
+    }
+}
