228ddfd9495277517d2b19dec040d01b657de4c2
elsa.m <manangelsa07@gmail.com>
2023-10-19 09:42:37 +0200

    add Auteur DTO

---
+++ TestAspApi/DTOs/AuteurDTO.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TestAspApi.DTOs
+{
+    public class AuteurDTO
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+    }
+}
