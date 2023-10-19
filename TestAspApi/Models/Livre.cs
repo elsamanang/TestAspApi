5f6607a2fc94b90c73489d7144407a72418f60e9
elsa.m <manangelsa07@gmail.com>
2023-10-19 09:44:15 +0200

    add livre model

---
+++ TestAspApi/Models/Livre.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TestAspApi.Models
+{
+    public class Livre
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public required string Title { get; set; }
+        public string? Description { get; set; }
+        [Required]
+        public int Pages { get; set; }
+        [Required]
+        public int AuteurId { get; set; }
+
+        public virtual Auteur? Auteur { get; set;}
+    }
+}
