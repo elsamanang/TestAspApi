2701220f7b157435fdbbf5d45f652e93ed547bd6
elsa.m <manangelsa07@gmail.com>
2023-10-21 08:37:15 +0200

    add genre dto

---
+++ TestAspApi/DTOs/GenreDTO.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TestAspApi.DTOs
+{
+#nullable disable
+    public class GenreDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
