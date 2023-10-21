778aeb8a6ee4dd27f0576e82d00f3b31e77bc7fc
elsa.m <manangelsa07@gmail.com>
2023-10-21 08:38:16 +0200

    create genre service interface

---
+++ TestAspApi/Services/Interface/IGenresService.cs
@@ -0,0 +1,16 @@
+﻿using TestAspApi.Commons;
+using TestAspApi.DTOs;
+using TestAspApi.Models;
+
+namespace TestAspApi.Services.Interface
+{
+    public interface IGenresService
+    {
+        Task<Reponse<IReadOnlyCollection<GenreDTO>>> GetAllAsync();
+        Task<Reponse<Genre>> GetGenreWithLivresAsync(int id);
+        Task<Reponse<GenreDTO>> GetOneGenreAsync(int id);
+        Task<Reponse<GenreDTO>> CreerNouveauGenreAsync(CreateGenreDTO creerGenre);
+        Task<Reponse<GenreDTO>> ModifierGenreAsync(int id, CreateGenreDTO modifierGenre);
+        Task<Reponse<GenreDTO>> SupprimerGenreAsync(int id);
+    }
+}
