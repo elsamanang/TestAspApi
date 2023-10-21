e2a252da443b882963e41576243225c1b4360782
elsa.m <manangelsa07@gmail.com>
2023-10-21 08:36:44 +0200

    add genre creation dto

---
+++ TestAspApi/DTOs/CreateGenreDTO.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TestAspApi.DTOs
+{
+#nullable disable
+    public class CreateGenreDTO
+    {
+        [MinLength(length: 3, ErrorMessage = "Le nom doit avoir 3 charateres ou plus")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Veuillez saisir le genre")]
+        public string Name { get; set; }
+    }
+}
