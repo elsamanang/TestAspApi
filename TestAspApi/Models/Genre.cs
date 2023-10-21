92e5680a39dfb502276c7d53d5cc31f065a2c937
elsa.m <manangelsa07@gmail.com>
2023-10-21 08:37:47 +0200

    add genre model

---
+++ TestAspApi/Models/Genre.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TestAspApi.Models
+{
+    public class Genre
+    {
+        public Genre() {
+            Livres = new HashSet<Livre>();
+        }
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public required string Name { get; set; }
+        public virtual ICollection<Livre> Livres { get; set; }
+    }
+}
