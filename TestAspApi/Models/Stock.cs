d6b3299e556ac76dba8357f81c8e6fa46c077b6b
elsa.m <manangelsa07@gmail.com>
2023-10-25 17:37:15 +0200

    add stock model

---
+++ TestAspApi/Models/Stock.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TestAspApi.Models
+{
+#nullable disable
+    public class Stock
+    {
+
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string LivreId { get; set; }
+        [Required]
+        public int Quantite { get; set; }
+
+        public virtual Livre Livre { get; set; }
+    }
+}
