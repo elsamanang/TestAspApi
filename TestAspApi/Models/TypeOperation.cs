06e83263ec2dec66c9f42b9550ac41ae3d66d997
elsa.m <manangelsa07@gmail.com>
2023-10-25 17:37:28 +0200

    type operation model

---
+++ TestAspApi/Models/TypeOperation.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TestAspApi.Models
+{
+#nullable disable
+    public class TypeOperation
+    {
+        public TypeOperation() { 
+            Operations = new HashSet<Operation>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+
+        public ICollection<Operation> Operations { get; set;}
+    }
+}
