7ddbebe3d330cd4d62c0d6f45e9ec265c9737eb1
elsa.m <manangelsa07@gmail.com>
2023-10-25 17:36:52 +0200

    add operation model

---
+++ TestAspApi/Models/Operation.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TestAspApi.Models
+{
+#nullable disable
+    public class Operation
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public int TypeOperationId { get; set; }
+        [Required]
+        public int Prix { get; set; }
+        [Required]
+        public int Quantite { get; set; }
+        [Required]
+        public DateTime Day { get; set; }
+
+        public virtual TypeOperation TypeOperation { get; set; }
+    }
+}
