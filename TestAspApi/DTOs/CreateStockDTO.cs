a27c7635af83a845ac9e9581ca127f362dfe062d
elsa.m <manangelsa07@gmail.com>
2023-11-02 13:53:02 +0100

    add create stock dto

---
+++ TestAspApi/DTOs/CreateStockDTO.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TestAspApi.DTOs
+{
+    public class CreateStockDTO
+    {
+        [Required(AllowEmptyStrings = false, ErrorMessage = "La quantité doit être doit mentionnée")]
+        public int Quantite { get; set; }
+        [Required]
+        public int LivreId { get; set; }
+    }
+}
