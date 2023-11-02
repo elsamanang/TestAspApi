c2e216a751b1aeb1fef98d4b13d7438b42dc1aa1
elsa.m <manangelsa07@gmail.com>
2023-11-02 13:53:22 +0100

    add create type operation dto

---
+++ TestAspApi/DTOs/CreateTypeOperationDTO.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TestAspApi.DTOs
+{
+#nullable disable
+    public class CreateTypeOperationDTO
+    {
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Le nom doit être doit mentionné")]
+        public string Name { get; set; }
+    }
+}
