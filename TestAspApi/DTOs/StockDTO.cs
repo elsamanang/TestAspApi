87c5a8e15f55216a645425f00e68ce77001cbe5d
elsa.m <manangelsa07@gmail.com>
2023-11-02 13:53:45 +0100

    add stock dto

---
+++ TestAspApi/DTOs/StockDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace TestAspApi.DTOs
+{
+#nullable disable
+    public class StockDTO
+    {
+        public int Id { get; set; }
+        public int Quantite { get; set; }
+        public int LivreId { get; set; }
+        public int LivreName { get; set;}
+    }
+}
