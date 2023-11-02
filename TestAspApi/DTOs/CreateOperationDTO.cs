91f323eb531d61a57d2a782fce642e330cc0d8ca
elsa.m <manangelsa07@gmail.com>
2023-11-02 13:52:49 +0100

    add create operation dto

---
+++ TestAspApi/DTOs/CreateOperationDTO.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TestAspApi.DTOs
+{
+#nullable disable
+    public class CreateOperationDTO
+    {
+        [Required(AllowEmptyStrings =false, ErrorMessage = "Le prix doit être mentionné")]
+        public int Prix { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "La quantité doit être mentionnée")]
+        public int Quantite { get; set; }
+        [Required]
+        public DateTime Day { get; set; }
+        [Required]
+        public int TypeOperationId { get; set; }
+    }
+}
