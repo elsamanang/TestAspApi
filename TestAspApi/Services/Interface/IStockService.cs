979b92909cffb0c7bd4cbaee1ecbba6dbbe025f9
elsa.m <manangelsa07@gmail.com>
2023-11-02 14:12:34 +0100

    create stock service interface

---
+++ TestAspApi/Services/Interface/IStockService.cs
@@ -0,0 +1,14 @@
+﻿using TestAspApi.Commons;
+using TestAspApi.DTOs;
+
+namespace TestAspApi.Services.Interface
+{
+    public interface IStockService
+    {
+        Task<Reponse<IReadOnlyCollection<StockDTO>>> GetAllAsync();
+        Task<Reponse<StockDTO>> GetOneAsync(int id);
+        Task<Reponse<StockDTO>> CreerNouveauStockAsync(CreateStockDTO createStockDTO);
+        Task<Reponse<StockDTO>> ModifierStockAsync(int id, CreateStockDTO updateStockDTO);
+        Task<Reponse<StockDTO>> SupprimerStockAsync(int id);
+    }
+}
