2aaf7bd5a5e31f1f84986b2731164b68b5c6db37
elsa.m <manangelsa07@gmail.com>
2023-11-02 14:12:23 +0100

    create operation service interface

---
+++ TestAspApi/Services/Interface/IOperationService.cs
@@ -0,0 +1,15 @@
+﻿using TestAspApi.Commons;
+using TestAspApi.DTOs;
+
+namespace TestAspApi.Services.Interface
+{
+    public interface IOperationService
+    {
+        Task<Reponse<IReadOnlyCollection<OperationDTO>>> GetAllAsync();
+        Task<Reponse<IReadOnlyCollection<OperationDTO>>> GetByDateAsync(DateTime day);
+        Task<Reponse<OperationDTO>> GetOneAsync(int id);
+        Task<Reponse<OperationDTO>> CreerNouvelleOperationAsync(CreateOperationDTO createOperationDTO);
+        Task<Reponse<OperationDTO>> ModifierOperationAsync(int id, CreateOperationDTO updateOperation);
+        Task<Reponse<OperationDTO>> SupprimerOperationAsync(int id);
+    }
+}
